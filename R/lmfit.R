320f3a7c7b09d0d730dd61b34ebb574fd8cf3188
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2022-12-16 20:24:01 +0100

    fix: change on workers count
    in the future this should be an argument of the funciton

--- R/lmfit.R
+++ R/lmfit.R
@@ -237,7 +237,7 @@ mrlm <- function(M,design=NULL,ndups=1,spacing=1,weights=NULL,...)
   
   ####################################################################################
   library(BiocParallel)
-  n_workers <- 8
+  n_workers <- 32
   options(MulticoreParam=MulticoreParam(workers=n_workers))
   
   FUN <- function(i){
