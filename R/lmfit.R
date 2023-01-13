83081976bd32cd65e8400b9df5e54bab40cd1a05
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-01-13 17:06:24 +0100

    Update lmfit.R

--- R/lmfit.R
+++ R/lmfit.R
@@ -238,7 +238,7 @@ mrlm <- function(M,design=NULL,ndups=1,spacing=1,weights=NULL,...)
   ####################################################################################
   library(BiocParallel)
   n_workers <- parallel::detectCores() - 2
-  # n_workers <- 32
+  n_workers <- 1
   options(MulticoreParam=MulticoreParam(workers=n_workers))
   
   FUN <- function(i){
